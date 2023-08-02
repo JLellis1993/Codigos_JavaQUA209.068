da739c68d298c135bb3ab8ade26ea7d7fbb15cc2
Jlellis1993git config --local user.name Jlellis1993git config --local user.name Jlellis1993 <Juglacial@yahoo.com.br>
2023-08-02 17:32:58 -0300

    subindo arquivos da primeira aula de java

---
+++ OlaMundo/src/principal/OlaMundo.java
@@ -0,0 +1,18 @@
+package principal;
+
+//classe principal
+public class OlaMundo {
+
+	//metodo principal onde executa o programa
+	public static void main(String[] args) {
+		
+        //imprime uma linha e ao final salta uma nova linha
+		//print= imprimir; In = new line;
+	System.out.println("Hello World, Olá mundo!");
+	}
+//comentario de uma linha
+	/*
+	*esse é um comentario 
+	*de multiplas linhas
+	*/
+}    
