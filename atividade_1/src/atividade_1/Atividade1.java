d628638843805b76aabc1c027d264954013cf9aa
Jlellis1993git config --local user.name Jlellis1993git config --local user.name Jlellis1993 <Juglacial@yahoo.com.br>
2023-08-03 14:12:44 -0300

    Atividade 1

---
+++ atividade_1/src/atividade_1/Atividade1.java
@@ -0,0 +1,26 @@
+package atividade_1;
+
+public class Atividade1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero1 = 29;
+		int  numero2= 33;
+
+		String  nome = "Julia";
+		String sobrenome = " Lellis";
+		char sexo= 'F';
+
+
+		boolean logica1=true;
+		boolean logica2=false;
+
+		System.out.println ("Meu nome completo: " + nome + sobrenome);
+		System.out.println ("variavel numero 1 = " + numero1);
+		System.out.println ("variavel numero 2 = " + numero2);
+		System.out.println ("variavel sexo = " + sexo);
+		System.out.println ("variavel logica 1 = " + logica1);
+		System.out.println ("variavel logica 2 = " + logica2);
+	}
+}
+
