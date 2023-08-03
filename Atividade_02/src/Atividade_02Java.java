64170fdada2be41ea8163ec3c4067c74b47758fa
Jlellis1993git config --local user.name Jlellis1993git config --local user.name Jlellis1993 <Juglacial@yahoo.com.br>
2023-08-03 17:33:22 -0300

    Atividade 02

---
+++ Atividade_02/src/Atividade_02Java.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Atividade_02Java {
+
+	public static void main(String[] args) {
+		
+		
+
+			
+		Scanner leia = new Scanner(System.in);
+			double numero;
+		
+	System.out.println("Por favor digite um número");
+	numero= leia.nextDouble();
+	
+	System.out.println ("O número digitado foi" + numero );
+	leia.close();
+	
+	
+	
+
+	}
+
+}
+
